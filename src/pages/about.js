234dceaa0a1f40cb2271889e2e8278760bbf0e3f
StefanoFochesatto <stefano.gfochesatto@gmail.com>
2021-06-10 11:10:45 -0800

    test

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const about = () => {
+    return (
+        <div>
+            <h1>Yes this is a quick about page test.</h1>
+        </div>
+    )
+};
+
+export default about;
